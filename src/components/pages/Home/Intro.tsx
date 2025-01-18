dc5888bfb2b71a55ef612899f3d9b6fcdec1325d
arzaqulmughny <zaarza03@gmail.com>
2025-01-19 06:59:14 +0700

    feat-intro: dynamic age, update bio

--- src/components/pages/Home/Intro.tsx
+++ src/components/pages/Home/Intro.tsx
@@ -1,29 +1,33 @@
-'use client';
-import H1 from '@/components/ui/typography/h1';
-import P from '@/components/ui/typography/p';
-import Typewriter from 'typewriter-effect';
+"use client";
+import H1 from "@/components/ui/typography/h1";
+import P from "@/components/ui/typography/p";
+import Typewriter from "typewriter-effect";
 
 const Intro = () => {
-    return (
-        <div className='flex flex-col gap-y-4'>
-            <H1 className='whitespace-pre-line'>Hello,</H1>
-            <H1 className='flex whitespace-break-spaces -z-10'>
-                {`I'm `}
-                <Typewriter
-                    options={{ loop: true, strings: ['Arza', 'Web Developer'], autoStart: true }}
-                    onInit={(typewriter) => {
-                        typewriter.start();
-                    }}
-                />
-            </H1>
+  const age = new Date().getFullYear() - 2003;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <H1 className="whitespace-pre-line">Hello,</H1>
+      <H1 className="flex whitespace-break-spaces -z-10">
+        {`I'm `}
+        <Typewriter
+          options={{
+            loop: true,
+            strings: ["Arza", "Web Developer"],
+            autoStart: true,
+          }}
+          onInit={(typewriter) => {
+            typewriter.start();
+          }}
+        />
+      </H1>
 
-            <P className='text-justify'>
-                A 20 years old web developer with expertise as a front-end and backend technologies. creating visually
-                appealing and highly functional websites. With a keen eye for detail and a commitment to delivering high
-                quality work.
-            </P>
-        </div>
-    );
+      <P className="text-justify">
+        A {age}-year-old web developer driven by crafting effective solutions
+        over merely showcasing technology.{" "}
+      </P>
+    </div>
+  );
 };
 
 export default Intro;
