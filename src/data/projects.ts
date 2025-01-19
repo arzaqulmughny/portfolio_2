8d6ff53dcf33295cfd31055caff3e74e80365d1c
arzaqulmughny <zaarza03@gmail.com>
2025-01-19 07:03:21 +0700

    feat-project: add new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,48 +1,58 @@
-import tProject from '@/types/tProject';
+import tProject from "@/types/tProject";
 
 const projects: tProject[] = [
-    {
-        categories: ['Web Application'],
-        title: 'Rembugan App',
-        summary: 'Realtime Chat Web Based Application',
-        url: 'https://github.com/zaarza/rembugan-app',
-        thumbnail: 'project-thumbnail-rembugan-app.png',
-    },
-    {
-        categories: ['Web Application'],
-        title: 'Tokoana',
-        summary: 'Point of Sales App for grocery store',
-        url: 'https://github.com/zaarza/tokoana',
-        thumbnail: 'project-thumbnail-tokoana.png',
-    },
-    {
-        categories: ['Final Project'],
-        title: 'Ecommerce Catalog',
-        summary: 'Final Project | Project-Based Virtual Intern : Front End Developer Core Initiative x Rakamin Academy',
-        url: 'https://github.com/zaarza/ecommerce-catalog',
-        thumbnail: 'project-thumbnail-ecommerce-catalog.png',
-    },
-    {
-        categories: ['Final Project', 'Web Application'],
-        title: 'Note App',
-        summary: 'Final Project | Dicoding Academy "Belajar Fundamental Aplikasi Web dengan React"',
-        url: 'https://github.com/zaarza/simple-note-app',
-        thumbnail: 'project-thumbnail-note-app.png',
-    },
-    {
-        categories: ['Final Project', 'Web Application'],
-        title: 'InfoResto',
-        summary: 'Final Project | Dicoding Academy "Menjadi Front-End Web Developer Expert"',
-        url: 'https://github.com/zaarza/restaurant-catalogue',
-        thumbnail: 'project-thumbnail-inforesto.png',
-    },
-    {
-        categories: ['UI Slicing'],
-        title: 'Rest Countries App',
-        summary: 'Solution for Frontendmentor challenge',
-        url: 'https://github.com/zaarza/rest-countries-app',
-        thumbnail: 'project-thumbnail-rest-countries-app.png',
-    },
+  {
+    categories: ["Web Application"],
+    title: "Sistem Informasi Perpustakaan",
+    summary: "Simplifying Library Records: Easier and More Efficient!",
+    url: "https://github.com/arzaqulmughny/sistem-informasi-perpustakaan",
+    thumbnail: "project-thumbnail-sip.png",
+  },
+  {
+    categories: ["Web Application"],
+    title: "Rembugan App",
+    summary: "Realtime Chat Web Based Application",
+    url: "https://github.com/arzaqulmughny/rembugan-app",
+    thumbnail: "project-thumbnail-rembugan-app.png",
+  },
+  {
+    categories: ["Web Application"],
+    title: "Tokoana",
+    summary: "Point of Sales App for grocery store",
+    url: "https://github.com/arzaqulmughny/tokoana",
+    thumbnail: "project-thumbnail-tokoana.png",
+  },
+  {
+    categories: ["Final Project"],
+    title: "Ecommerce Catalog",
+    summary:
+      "Final Project | Project-Based Virtual Intern : Front End Developer Core Initiative x Rakamin Academy",
+    url: "https://github.com/arzaqulmughny/ecommerce-catalog",
+    thumbnail: "project-thumbnail-ecommerce-catalog.png",
+  },
+  {
+    categories: ["Final Project", "Web Application"],
+    title: "Note App",
+    summary:
+      'Final Project | Dicoding Academy "Belajar Fundamental Aplikasi Web dengan React"',
+    url: "https://github.com/arzaqulmughny/simple-note-app",
+    thumbnail: "project-thumbnail-note-app.png",
+  },
+  {
+    categories: ["Final Project", "Web Application"],
+    title: "InfoResto",
+    summary:
+      'Final Project | Dicoding Academy "Menjadi Front-End Web Developer Expert"',
+    url: "https://github.com/arzaqulmughny/restaurant-catalogue",
+    thumbnail: "project-thumbnail-inforesto.png",
+  },
+  {
+    categories: ["UI Slicing"],
+    title: "Rest Countries App",
+    summary: "Solution for Frontendmentor challenge",
+    url: "https://github.com/arzaqulmughny/rest-countries-app",
+    thumbnail: "project-thumbnail-rest-countries-app.png",
+  },
 ];
 
 export default projects;
